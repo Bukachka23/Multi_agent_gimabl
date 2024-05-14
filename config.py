6f27cab3cdcee80c9f3e4735ad3dc82d537be333
Ihor Tresnytskyi <magnumgrizzly2000@gmail.com>
2024-05-14 13:02:22 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,95 @@
+import json
+import logging
+from os import path
+
+DEFAULT_FILE_PATH = "multi_agent_equipment/config/config.json"
+
+class Configuration(object):
+    
+    KEY_MOTOR_CLASS = "motor-class"
+    VALUE_MOTOR_CLASS_LOCAL = "local"
+    VALUE_MOTOR_CLASS_DUMMY = "dummy"
+    
+    KEY_IMU_CLASS = "imu-class"
+    VALUE_IMU_CLASS_6050 = "imu6050"
+    VALUE_IMU_CLASS_6050_DMP = "imu6050_dmp"
+    VALUE_IMU_CLASS_DUMMY = "dummy"
+    
+    PID_PERIOD = "pid-period"
+    
+    PID_KP = "PID_KP"
+    PID_KI = "PID_KI"
+    PID_KD = "PID_KD"
+    
+    DEFAULT_CONFIG = {
+                      KEY_MOTOR_CLASS: VALUE_MOTOR_CLASS_DUMMY,
+                      KEY_IMU_CLASS: VALUE_IMU_CLASS_DUMMY,
+                      
+                      PID_PERIOD: 0.1,
+                      PID_KP: [0.0, 0.0],  
+                      PID_KI: [0.0, 0.0],  
+                      PID_KD: [0.0, 0.0]
+                      }
+    
+    _instance = None
+    
+    @staticmethod
+    def getInstance():
+        """
+        @return: Unique object instance
+        """
+        
+        if Configuration._instance == None:
+            Configuration._instance = Configuration()
+            
+        return Configuration._instance
+    
+
+    def __init__(self):
+        """
+        Constructor
+        """        
+        
+        self._config = Configuration.DEFAULT_CONFIG.copy()
+            
+    
+    def read(self, filepath=DEFAULT_FILE_PATH):
+        """
+        Reads stored configuration from file
+        @param filepath: Configuration filepath
+        """
+        
+        if path.exists(filepath):
+    
+            with open(filepath, "r") as configFile:
+                serializedConfig = " ".join(configFile.readlines())
+                configFile.close()
+                
+            storedConfig = json.loads(serializedConfig)
+            
+            #Replace default config by stored config
+            for key in self._config.keys():
+                
+                if key in storedConfig:
+                    
+                    self._config[key] = storedConfig[key]
+        else:
+            logging.info("Configuration file {0} not found. Using default config.".format(filepath))            
+                    
+                    
+    def save(self, filepath=DEFAULT_FILE_PATH):
+        """
+        Writes current configuration into file
+        @param filepath: Configuration filepath
+        """
+        
+        serializedConfig = json.dumps(self._config)        
+        with open(filepath, "w+") as configFile:
+            configFile.write(serializedConfig + "\n")
+            configFile.close()
+            
+        
+            
+    def getConfig(self):
+        
+        return self._config
---
+++ run_agent.py
@@ -0,0 +1,32 @@
+import logging
+from time import sleep
+from config import Configuration
+from multi_agent_equipment.core_servo.driver import Driver
+from multi_agent_equipment.core_stabilization.stabilizator import Stabilizator
+
+
+def main():
+
+    logging.basicConfig(level=logging.INFO)
+    
+    configManager = Configuration.getInstance()
+    configManager.read()
+    configManager.save()
+    
+    config = configManager.getConfig()
+            
+    stabilizator = Stabilizator(config[Configuration.KEY_IMU_CLASS], Driver(2, config[Configuration.KEY_MOTOR_CLASS]), config[Configuration.PID_PERIOD], 2)
+    stabilizator.setPidConstants(config[Configuration.PID_KP], config[Configuration.PID_KI], config[Configuration.PID_KD])
+    stabilizator.start()
+    print ("started!")
+    logging.info("Started! Press Ctrl+C to stop.")
+    
+    try:
+        while True:
+            sleep(0.2)
+    except:
+        stabilizator.stop() 
+
+
+if __name__ == '__main__':
+    main()
